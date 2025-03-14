c71f530976c2e01643f7691a40fc022d743d855c
Zeeshan Ahmed <dev.zeeshanahmed@gmail.com>
2025-03-14 12:07:27 -0700

    path error fixed

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import './home.scss'
 import HeroBanner from './heroBanner/HeroBanner'
-import Trending from './trending/trending'
+import Trending from './trending/Trending'
 import Popular from './popular/Popular'
 import TopRated from './topRated/TopRated'
 
